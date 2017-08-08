402759f00b12cb03b30c571297e16c1a19c0c91a
HannaJeon <jks7psj@gmail.com>
2017-08-08 11:18:46 +0900

    SortView xib 생성

---
+++ iOS/BaeminMap/BaeminMap/SortView.swift
@@ -0,0 +1,32 @@
+//
+//  SortView.swift
+//  BaeminMap
+//
+//  Created by woowabrothers on 2017. 8. 8..
+//  Copyright © 2017년 HannaJeon. All rights reserved.
+//
+
+import UIKit
+
+class SortView: UIView {
+    
+    @IBOutlet weak var nearButton: UIButton!
+    @IBOutlet weak var orderButton: UIButton!
+    @IBOutlet weak var reviewButton: UIButton!
+    @IBOutlet weak var popularButton: UIButton!
+    @IBOutlet weak var bookmarkButton: UIButton!
+    
+    override func layoutSubviews() {
+        nearButton.titleLabel?.textColor = UIColor.white
+        nearButton.
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
