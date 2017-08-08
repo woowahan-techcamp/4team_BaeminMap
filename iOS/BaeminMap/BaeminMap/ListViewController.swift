08e3c42a07a4c31445af52e90d7ac8a72b11be3a
lmjing <lmjing1234@gmail.com>
2017-08-08 17:07:45 +0900

    리스트뷰에 Cell 띄우기 임의 테스트

--- iOS/BaeminMap/BaeminMap/ListViewController.swift
+++ iOS/BaeminMap/BaeminMap/ListViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ListViewController: UIViewController {
 
+    @IBOutlet weak var listView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        listView.delegate = self
+        listView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +34,20 @@ class ListViewController: UIViewController {
     }
     */
 
+}
+
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = Bundle.main.loadNibNamed("ListTableViewCell", owner: self, options: nil)?.first as! ListTableViewCell
+        tableView.rowHeight = cell.frame.height
+        return cell
+    }
 }
