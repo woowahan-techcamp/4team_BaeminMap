8b4b0dbbabfc559da7b5e613f1294a55d04eccfd
lmjing <lmjing1234@gmail.com>
2017-08-08 15:48:03 +0900

    ListTableViewCell Xib rating 제외하고 셋팅

---
+++ iOS/BaeminMap/BaeminMap/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  BaeminMap
+//
+//  Created by woowabrothers on 2017. 8. 8..
+//  Copyright © 2017년 HannaJeon. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
