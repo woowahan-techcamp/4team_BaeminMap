42e84b338f5ca437dd732d1764403468e1b28fe4
lmjing <lmjing1234@gmail.com>
2017-08-08 17:34:46 +0900

    distanceLabel 추가 및 Xib 와 UITableViewCell outlet 연결

--- iOS/BaeminMap/BaeminMap/ListTableViewCell.swift
+++ iOS/BaeminMap/BaeminMap/ListTableViewCell.swift
@@ -7,16 +7,24 @@
 //
 
 import UIKit
+import Cosmos
 
 class ListTableViewCell: UITableViewCell {
     
-    @IBOutlet var ShopImageView: UIImageView!
+    @IBOutlet var shopImageView: UIImageView!
+    @IBOutlet var ratingView: CosmosView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var reviewLabel: UILabel!
+    @IBOutlet var ownerReviewLabel: UILabel!
+    @IBOutlet var baropayLabel: UILabel!
+    @IBOutlet var meetingpayLabel: UILabel!
+    @IBOutlet var distanceLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        ShopImageView.layer.cornerRadius = (ShopImageView.frame.size.height) / 2
-        ShopImageView.layer.masksToBounds = true
+        shopImageView.layer.cornerRadius = (shopImageView.frame.size.height) / 2
+        shopImageView.layer.masksToBounds = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
