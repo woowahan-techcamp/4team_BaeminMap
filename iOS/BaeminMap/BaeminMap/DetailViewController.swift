713dfbca47a0a7801e7ea1812b2d03425ac6dc1e
lmjing <lmjing1234@gmail.com>
2017-08-09 17:26:48 +0900

    collectionView scroll 막고 전체 scroll 실행,collectionView 및 ScrollView  height 설정

--- iOS/BaeminMap/BaeminMap/DetailViewController.swift
+++ iOS/BaeminMap/BaeminMap/DetailViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var collectionView: UICollectionView!
     
     override func viewDidLoad() {
@@ -17,6 +18,8 @@ class DetailViewController: UIViewController {
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        initView()
 
         // Do any additional setup after loading the view.
     }
@@ -26,21 +29,17 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func initView() {
     }
-    */
 }
 
 extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+        
+        //cell의 전체 개수와 cell의 height를 알고 있어 initView에 미리 해놓으려했으나 각 행 사이의 공백의 높이를 알지 못해 유동적으로 height을 변경함
+        collectionView.frame = CGRect(x: collectionView.frame.minX, y: collectionView.frame.minY, width: view.frame.width, height: collectionView.contentSize.height)
+        scrollView.contentSize.height = 325 + collectionView.frame.height
         
         return cell
     }
