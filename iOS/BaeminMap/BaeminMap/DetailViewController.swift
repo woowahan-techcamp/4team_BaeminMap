a01265a1013fb4ba0eb2345b3b6b56038a75839e
lmjing <lmjing1234@gmail.com>
2017-08-09 17:56:08 +0900

    collectionView 간격 줌

--- iOS/BaeminMap/BaeminMap/DetailViewController.swift
+++ iOS/BaeminMap/BaeminMap/DetailViewController.swift
@@ -38,7 +38,7 @@ extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSo
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
         
         //cell의 전체 개수와 cell의 height를 알고 있어 initView에 미리 해놓으려했으나 각 행 사이의 공백의 높이를 알지 못해 유동적으로 height을 변경함
-        collectionView.frame = CGRect(x: collectionView.frame.minX, y: collectionView.frame.minY, width: view.frame.width, height: collectionView.contentSize.height)
+        collectionView.frame = CGRect(x: collectionView.frame.minX, y: collectionView.frame.minY, width: collectionView.contentSize.width, height: collectionView.contentSize.height)
         scrollView.contentSize.height = 325 + collectionView.frame.height
         
         return cell
