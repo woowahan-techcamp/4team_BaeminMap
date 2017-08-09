3d2f91f1d80d4d16694dabc7b245a4436bdd429d
lmjing <lmjing1234@gmail.com>
2017-08-09 20:29:19 +0900

    TableView 추가 및 기본 연동

--- iOS/BaeminMap/BaeminMap/DetailViewController.swift
+++ iOS/BaeminMap/BaeminMap/DetailViewController.swift
@@ -12,12 +12,15 @@ class DetailViewController: UIViewController {
 
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
         
         initView()
 
@@ -38,13 +41,26 @@ extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSo
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
         
         //cell의 전체 개수와 cell의 height를 알고 있어 initView에 미리 해놓으려했으나 각 행 사이의 공백의 높이를 알지 못해 유동적으로 height을 변경함
-        collectionView.frame = CGRect(x: collectionView.frame.minX, y: collectionView.frame.minY, width: collectionView.contentSize.width, height: collectionView.contentSize.height)
-        scrollView.contentSize.height = 325 + collectionView.frame.height
+        collectionView.frame = CGRect(x: 0, y: collectionView.frame.minY, width: collectionView.contentSize.width, height: collectionView.contentSize.height)
+        tableView.frame = CGRect(x: 0, y: collectionView.frame.maxY, width: tableView.contentSize.width, height: tableView.contentSize.height)
+        scrollView.contentSize.height = tableView.frame.maxY
         
         return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 6
+    }
+}
+
+extension DetailViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+        
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
 }
