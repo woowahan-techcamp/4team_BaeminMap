83abac30087f143bb4d45ee8262590ccc4e16433
HannaJeon <jks7psj@gmail.com>
2017-08-14 16:39:48 +0900

    전체 업소목록 request 후 json으로 한번에 전달하는 라우터 생성

--- server/app.js
+++ server/app.js
@@ -3,14 +3,41 @@ var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
 var config = require('./config');
+var eachAsync = require('each-async');
 
-app.listen(3000, function() {
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(3000, ()=> {
   console.log('start 3000port!')
 })
 
-app.get('/', function(req, res) {
-  config.getToken()
-  console.log(config.token)
-  res.send('success')
 
+app.post('/shops', (req, res)=> {
+  var shops = {}
+  var shopArray = []
+  config.getToken(()=> {
+    eachAsync(config.categoryList, function(category, index, done) {
+      request({
+        url: config.standardUrl+'/v2/shops?size=2000',
+        method: 'POST',
+        json: true,
+        headers: {
+          'Authorization': 'Bearer '+config.token
+        },
+        body: {
+          'lat': req.body.lat,
+          'lng': req.body.lng,
+          'category': category
+        }
+      }, function(err, res, body) {
+        shopArray = shopArray.concat(body.content)
+        shops[category] = body.content
+        done()
+      })
+    }, function(err) {
+      if(err) throw err;
+      res.json({shops: shops, shopArray: shopArray})
+    })
+  })
 })
