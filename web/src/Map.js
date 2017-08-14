ee8b8565bc7e15de68b31544d9e39fd3baa8a54c
beomi <latheledusjp@gmail.com>
2017-08-14 15:24:33 +0900

    Update: 위치 변경시 새로 업소 목록/마커 보여줌

--- web/src/Map.js
+++ web/src/Map.js
@@ -1,24 +1,23 @@
 import ShopDetail from './ShopDetail'
+import ShopList from './ShopList'
 
 class Map {
-    constructor() {
+    constructor(data, token) {
         this.infowindow;
         this.currentLocation = {lat: 37.5759879, lng: 126.9769229};
-        this.initMap();
-        this.searchPosition();
-    }
-
-    updatePosition(position) {
-        this.setUserMarker(position)
-        this.map.setCenter(position)
-        this.currentLocation = position
-    }
-
-    initMap() {
         this.map = new google.maps.Map(document.getElementById('map'), {
             zoom: 15,
             center: this.currentLocation
         });
+        this.searchPosition();
+        this.data = data
+        this.token = token
+    }
+
+    updatePosition(position) {
+        this.setUserMarker(position)
+        this.map.setCenter(position);
+        this.currentLocation = position
     }
 
     setUserMarker(position) {
@@ -33,17 +32,22 @@ class Map {
         const map = this.map;
         const input = document.getElementById('pac-input');
         const searchBox = new google.maps.places.SearchBox(input);
-        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
         // Bias the SearchBox results towards current map's viewport.
-        this.map.addListener('bounds_changed', () => {
-            searchBox.setBounds(this.map.getBounds());
+        map.addListener('bounds_changed', () => {
+            searchBox.setBounds(map.getBounds());
+            const pos = {
+                lat: this.map.center.lat(),
+                lng: this.map.center.lng()
+            };
+            this.reloadMap(pos, this.data, this.token)
         });
 
         let markers = [];
         // Listen for the event fired when the user selects a prediction and retrieve
         // more details for that place.
-        searchBox.addListener('places_changed', function () {
+        searchBox.addListener('places_changed', () => {
             const places = searchBox.getPlaces();
 
             if (places.length === 0) {
@@ -58,7 +62,7 @@ class Map {
 
             // For each place, get the icon, name and location.
             const bounds = new google.maps.LatLngBounds();
-            places.forEach(function (place) {
+            places.forEach((place) => {
                 if (!place.geometry) {
                     console.log("Returned place contains no geometry");
                     return;
@@ -109,6 +113,22 @@ class Map {
                 this.infowindow = infowindow
             });
         });
+    }
+
+    reloadMap(pos, data, token) {
+        // const pos = {
+        //     lat: this.map.center.lat(),
+        //     lng: this.map.center.lng()
+        // };
+        this.updatePosition(pos)
+        console.log('reloadMap data: ', data)
+        data.getShopList(data.categoryArr, pos, token).then((arr) => {
+            //필터 로직 들어갈 부분. 필터 이후 소트를 한다.
+            return data.sortList(arr, 0);
+        }).then((filteredData) => {
+            new ShopList("#shopListTemplate", "#shopList", filteredData)
+            this.setShopMarker(filteredData)
+        })
     }
 }
 
--- web/src/index.js
+++ web/src/index.js
@@ -40,13 +40,13 @@ function getToken() {
     return apiToken
 }
 
-function filterEventOn(tar){
+function filterEventOn(tar) {
     const eventHTML = document.querySelector(tar);
     eventHTML.addEventListener("click", function (e) {
         let target = e.target;
-        if (target.className === "category selected"){
+        if (target.className === "category selected") {
             target.className = "category"
-        } else if (target.className === "category"){
+        } else if (target.className === "category") {
             target.className = "category selected"
         }
 
@@ -56,75 +56,16 @@ function filterEventOn(tar){
 
 document.addEventListener('DOMContentLoaded', () => {
     const token = getToken()
-    const map = new Map();
-    const data = new Data();
-
     navigator.geolocation.getCurrentPosition((position) => {
+        const data = new Data();
+        const map = new Map(data, token);
         const pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
-        };
-        map.updatePosition(pos)
-        data.getShopList(data.categoryArr, pos, token).then((arr) => {
-            //필터 로직 들어갈 부분. 필터 이후 소트를 한다.
-            return data.sortList(arr, 0);
-        }).then((filteredData) => {
-            console.log(filteredData)
-            new ShopList("#shopListTemplate", "#shopList", filteredData)
-            map.setShopMarker(filteredData)
-        })
+        }
+        console.log('get position: ', pos)
+
+        map.reloadMap(pos, data, token)
     })
     filterEventOn(".category-list")
 })
-
-
-//     // 위치 가져오기(geoloaction)
-//     getlocation().then(
-//         axios.get(
-//             // 위치 받고 위치 근처 업소 넘기고
-//             // 위치 받아서 지도 위치 변경
-//         ).then(
-//             // TODO: 업소 목록을 필터링해서 넘겨주기
-//         ).then(
-//             // 업소 리스트를 업데이트
-//             // map 마커 업데이트
-//         ).then(
-//             // TODO: 위에 두개 관계 연결
-//         )
-//     )
-//
-// })
-//
-// 도큐먼트.이벤트(폼(조건)
-// 변경시
-// )
-// {
-//     // 업소 목록을 필터링해서 넘겨주기
-// )
-// .
-// then(
-//     // 위치기반 업소 리스트를 업데이트
-//     // map 마커 업데이트
-// ).then(
-//     // 위에 두개 관계 연결
-// )
-// }
-//
-// 위치
-// 변경시(검색시)
-// {
-//     // 위치 가져오기(검색한 결과)
-//     getlocation().then(
-//         axios.get(
-//             // 위치 받고 위치 근처 업소 넘기고
-//             // 위치 받아서 지도 위치 변경
-//         ).then(
-//             // 업소 목록을 필터링해서 넘겨주기
-//         ).then(
-//             // 위치기반 업소 리스트를 업데이트
-//             // map 마커 업데이트
-//         ).then(
-//             // 위에 두개 관계 연결
-//         )
-//     )
-// }
