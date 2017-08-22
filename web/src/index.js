8273ee6eeadddf6f5ce053eca4dc1a3e2f298688
Beomi <jun@beomi.net>
2017-08-22 16:04:01 +0900

    Revert "Web card"

--- web/src/index.js
+++ web/src/index.js
@@ -47,20 +47,14 @@ function filterEvent(arr, filter, layer, map, pos, apidata, condition) {
         const target = document.querySelector(e)
         target.addEventListener("click", function () {
             if (e === arr[0]) {
-                // 필터 화면 보기 버튼
                 filterSection.classList.add('show')
-                overLayer.classList.add('show')
                 filterChecker = document.querySelectorAll(".option.selected");
             } else if (e === arr[1]) {
-                // 필터 내 취소 버튼
                 filterSection.classList.remove('show')
-                overLayer.classList.remove('show')
                 //필터링 취소버튼을 누르면 필터값이 초기화된다.
                 filterReset(filterChecker, [".category", ".sort-option", "distance-option"]);
             } else if (e === arr[2]) {
-                //필터 내 적용버튼
                 filterSection.classList.remove('show')
-                overLayer.classList.remove('show')
                 //현재의 필터/정렬 옵션을 저장한다
                 filterChecker = document.querySelectorAll(".option.selected");
                 //TODO : 여기에 필터 적용해서 소트 요청하는 로직 구현
@@ -79,7 +73,7 @@ function filterEvent(arr, filter, layer, map, pos, apidata, condition) {
                 const distanceElement = document.querySelector('.distance-option-list > .selected')
                 const distance = parseFloat(distanceElement.dataset['distance'])
                 //shop-list 스크롤 초기화
-                map.reloadMap(distance, (map.currentLocation)? map.currentLocation:pos, apidata, condition, null, categoryList)
+                map.reloadMap(distance, pos, apidata, condition, null, categoryList)
             }
         })
     })
@@ -101,6 +95,7 @@ function sortByOption(tar, option) {
 
 function toggleCSSOnClick(el, target, css) {
     document.querySelector(el).addEventListener('click', (e) => {
+        e.preventDefault()
         const list = document.querySelector(target)
         if (list.classList.contains(css)) {
             list.classList.remove(css)
@@ -110,68 +105,37 @@ function toggleCSSOnClick(el, target, css) {
     })
 }
 
-// document.addEventListener('DOMContentLoaded', () => {
-const options = {
-    enableHighAccuracy: false,
-    timeout: 50,
-    maximumAge: Infinity
-};
-
-const indicator = document.querySelector('#indicator')
-let apidata;
-let map;
-
-navigator.geolocation.getCurrentPosition((position) => {
-    const pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-    }
-    apidata = new ApiData(pos);
-    map = new Map(apidata);
-    // Default search range: 300m(0.3km)
-    const distanceElement = document.querySelector('.distance-option-list > .selected')
-    const distance = parseFloat(distanceElement.dataset['distance'])
-    // Default Condition: distance
-    let condition = 'distance'
-    // Get all data and render them
-    map.reloadMap(distance, pos, apidata, condition)
-    categoryFilterEvent(".category-list");
-    sortByOption(".sort-option-list", "sort");
-    sortByOption(".distance-option-list", condition);
-    filterEvent(
-        [".filter-button-wrapper", ".cancel-filter-button", ".apply-filter-button"],
-        ".filter-controller",
-        ".layer",
-        map,
-        pos,
-        apidata,
-        condition
-    );
+document.addEventListener('DOMContentLoaded', () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }
+        const apidata = new ApiData(pos);
+        const map = new Map(apidata);
+        // Default search range: 300m(0.3km)
+        const distanceElement = document.querySelector('.distance-option-list > .selected')
+        const distance = parseFloat(distanceElement.dataset['distance'])
+        // Default Condition: distance
+        let condition = 'distance'
+        // Get all data and render them
+        map.reloadMap(distance, pos, apidata, condition)
+        categoryFilterEvent(".category-list");
+        sortByOption(".sort-option-list", "sort");
+        sortByOption(".distance-option-list", condition);
+        filterEvent(
+            [".filter-button-wrapper", ".cancel-filter-button", ".apply-filter-button"],
+            ".filter-controller",
+            ".layer",
+            map,
+            pos,
+            apidata,
+            condition
+        );
+    })
+    // Add Events on Click
+    // click el, target, css class
+    toggleCSSOnClick('#listOnOff', '#list', 'hidden')
+    toggleCSSOnClick('#filterOnOff', '.filter-controller', 'show')
 })
-// Add Events on Click
-// click el, target, css class
-toggleCSSOnClick('#listOnOff', '#list', 'mobile-hidden')
-toggleCSSOnClick('#filterOnOff', '.filter-controller', 'show')
-document.querySelector("#listOnOff").addEventListener("click", ()=>{
-    const card = document.querySelector("#card")
-    const floatButton = document.querySelector('.floating-button')
-    if(card.style.display === "block"){
-        card.style.display = "none";
-        floatButton.style.bottom = "60px";
-    }
-})
-document.querySelector("#card").addEventListener("click", (e)=>{
-    const shopNumber = e.target.dataset.shopnumber;
-    const data = map.filteredData.filter((i) => {return i.shopNumber==shopNumber})[0]
-    map.showModal(shopNumber, data, apidata)
-})
-// })
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    const modal = document.getElementById('modal');
-    const span = document.getElementsByClassName("close")[0];
-    if (event.target === modal || event.target === span) {
-        modal.style.display = "none";
-    }
-}
