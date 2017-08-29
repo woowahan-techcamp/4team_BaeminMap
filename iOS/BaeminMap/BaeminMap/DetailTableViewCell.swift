75df870b0043a0e5a1d94f611c897a61324f87d6
lmjing <lmjing1234@gmail.com>
2017-08-29 10:17:53 +0900

    DetailView Cell 메뉴명 아래 마진 적용

--- iOS/BaeminMap/BaeminMap/DetailTableViewCell.swift
+++ iOS/BaeminMap/BaeminMap/DetailTableViewCell.swift
@@ -15,7 +15,6 @@ class DetailTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
