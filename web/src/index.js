e93888d2060f78382b0c5387e95e77e9d15168ce
beomi <latheledusjp@gmail.com>
2017-08-30 10:10:05 +0900

    Update: marker redraw on click window

--- web/src/index.js
+++ web/src/index.js
@@ -208,6 +208,18 @@ function cardClickListener() {
 
 cardClickListener()
 
+const toggleFloatingButtonLocation = (floatButton, visibleTarget) => {
+    if (visibleTarget.style.display !== 'block') {
+        return
+    }
+    const floatButtonBottom = parseInt(floatButton.style.bottom.replace("px", ""))
+    if (floatButtonBottom > 100) {
+        floatButton.style.bottom = "40px";
+    } else {
+        floatButton.style.bottom = "140px";
+    }
+}
+
 //When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     const modal = document.getElementById('modal');
@@ -222,4 +234,13 @@ window.onclick = function (event) {
             map.resetHiddenList();
         }
     }
+    for (const marker of map.markers) {
+        if (marker.icon.url !== marker.categoryIcon.url) {
+            marker.setIcon(marker.categoryIcon)
+        }
+    }
+    const floatButton = document.querySelector('.floating-button')
+    const visibleTarget = document.querySelector('#card')
+    toggleFloatingButtonLocation(floatButton, visibleTarget)
+    visibleTarget.style.display = 'none'
 }
